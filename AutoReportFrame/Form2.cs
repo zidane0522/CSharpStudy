ea183c5544b657914501d66a1f4559c7ed026539
zidanepc <zidanepc@DESKTOP-VIIUN1E>
2017-06-23 15:09:44 +0800

    take over

--- AutoReportFrame/Form2.cs
+++ AutoReportFrame/Form2.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using mshtml;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,6 +17,12 @@ namespace AutoReportFrame
         {
             InitializeComponent();
             this.webBrowser1.Url = new Uri("http://wssq.saic.gov.cn:9080/tmsve/");
+            this.FormClosing += Form2_FormClosing;
+        }
+
+        private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.webBrowser1.Dispose();
         }
 
         public HtmlDocument doc { get; set; }
@@ -24,14 +31,48 @@ namespace AutoReportFrame
 
         private void button1_Click(object sender, EventArgs e)
         {
-            doc = webBrowser1.Document;
-            ele = doc.GetElementById("codefans_net").FirstChild ;
-            object ddd= ele.InvokeMember("onclick");
+            //doc = webBrowser1.Document;
+            ////ele = doc.GetElementById("codefans_net").FirstChild ;
+            ////object ddd= ele.InvokeMember("onclick");
+            //object obj = "/tmsve/commonGoods_getIntCls.xhtml";
+            //doc.InvokeScript("popUpWindow",new object[] { "/tmsve/commonGoods_getIntCls.xhtml" });
+            ////popUpWindow('/tmsve/commonGoods_getIntCls.xhtml')
+
+            this.webBrowser1.Navigate("http://wssq.saic.gov.cn:9080/tmsve/sbzcsq_getSbzcMain.xhtml");
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            //ele.
+            //if (doc!=null)
+            //{
+            //    IHTMLDocument2 hdoc =doc.DomDocument as IHTMLDocument2;
+            //    IHTMLWindow2 win = hdoc.parentWindow as mshtml.IHTMLWindow2;
+            //    var d= win.execScript(@"function sucdd(){ return popUpWin;}", "javascript");
+            //}
+            //var dddd = doc.InvokeScript("sucdd");
+            ////mshtml.htmlWindow2class
+            try
+            {
+                doc = webBrowser1.Document;
+                //HtmlElementCollection dd = doc.GetElementsByTagName("a");
+                //foreach (HtmlElement item in dd)
+                //{
+                //    if (item.InnerText.Contains("【点击添加商品"))
+                //    {
+                //        item.InvokeMember("click");
+                //        break;
+                //    }
+                //}
+
+                object obj = "/tmsve/commonGoods_getIntCls.xhtml";
+                doc.InvokeScript("popUpWindow", new object[] { "/tmsve/commonGoods_getGoods.xhtml?code=0506&id=2CA2DB43AF7DC0A2E0537F000001C0A2" });
+            }
+            catch (Exception ex)
+            {
+
+            }
+    
+
         }
     }
 }

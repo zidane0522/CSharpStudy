975c46ac6031c3b584c000bda91d2e8d2e5d4885
zidane0522 <lishen1990@163.com>
2017-07-10 17:36:59 +0800

    d

--- AutoReportFrame/Menu.Designer.cs
+++ AutoReportFrame/Menu.Designer.cs
@@ -36,9 +36,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(99, 45);
+            this.button1.Location = new System.Drawing.Point(66, 30);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(290, 66);
+            this.button1.Size = new System.Drawing.Size(193, 44);
             this.button1.TabIndex = 0;
             this.button1.Text = "自动上报";
             this.button1.UseVisualStyleBackColor = true;
@@ -46,41 +47,45 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(99, 144);
+            this.button2.Location = new System.Drawing.Point(66, 96);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(290, 66);
+            this.button2.Size = new System.Drawing.Size(193, 44);
             this.button2.TabIndex = 1;
-            this.button2.Text = "申请管理";
+            this.button2.Text = "商标申请信息管理";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(99, 347);
+            this.button3.Location = new System.Drawing.Point(66, 231);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(290, 66);
+            this.button3.Size = new System.Drawing.Size(193, 44);
             this.button3.TabIndex = 3;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(99, 248);
+            this.button4.Location = new System.Drawing.Point(66, 165);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(290, 66);
+            this.button4.Size = new System.Drawing.Size(193, 44);
             this.button4.TabIndex = 2;
-            this.button4.Text = "button4";
+            this.button4.Text = "11111";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // Menu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(477, 478);
+            this.ClientSize = new System.Drawing.Size(318, 319);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Menu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Menu";
